a0f196dff05713ef17f2ba92c00fedaa1f41f723
Lorik Morina <lorik.morina1@student.uni-pr.edu>
2023-05-02 08:06:04 +0200

    loginDesign

--- src/main/java/com/example/knkprojectgr13/logInController.java
+++ src/main/java/com/example/knkprojectgr13/logInController.java
@@ -1,14 +1,21 @@
 package com.example.knkprojectgr13;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class logInController {
     @FXML
     private Label welcomeText;
-
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField txtUsername;
+    @FXML
+    private PasswordField pwdPassword;
+    @FXML
+    private void btnLoginClick(ActionEvent event) {
+
     }
+
 }
