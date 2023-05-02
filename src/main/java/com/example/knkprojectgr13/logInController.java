debf008f53bca5db36ec5299c582e17d4d7b1501
Lorik Morina <lorik.morina1@student.uni-pr.edu>
2023-05-02 14:21:25 +0200

    link logic

--- src/main/java/com/example/knkprojectgr13/logInController.java
+++ src/main/java/com/example/knkprojectgr13/logInController.java
@@ -2,9 +2,16 @@ package com.example.knkprojectgr13;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class logInController {
     @FXML
@@ -16,6 +23,14 @@ public class logInController {
     @FXML
     private void btnLoginClick(ActionEvent event) {
 
+    }
+    @FXML
+    void signupBtn(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("signup.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
--- src/main/java/com/example/knkprojectgr13/signUpController.java
+++ src/main/java/com/example/knkprojectgr13/signUpController.java
@@ -2,10 +2,17 @@ package com.example.knkprojectgr13;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class signUpController {
     @FXML
@@ -27,5 +34,12 @@ public class signUpController {
     private void btnLoginClick(ActionEvent event) {
 
     }
-
+    @FXML
+    void loginBtn(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
