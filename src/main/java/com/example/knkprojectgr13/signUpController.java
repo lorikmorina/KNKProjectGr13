28bb0fb94ba969cd02f1221a27da74ca3a1203ab
Lorik Morina <lorik.morina1@student.uni-pr.edu>
2023-05-02 08:16:57 +0200

    signupDesign

--- src/main/java/com/example/knkprojectgr13/signUpController.java
+++ src/main/java/com/example/knkprojectgr13/signUpController.java
@@ -1,8 +1,31 @@
 package com.example.knkprojectgr13;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class signUpController {
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtFullName;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtPersonalNr;
+
+    @FXML
+    private void btnLoginClick(ActionEvent event) {
+
+    }
 
 }
