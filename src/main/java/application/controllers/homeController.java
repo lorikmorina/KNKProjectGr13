430e7180b135040ab63beff44494dfd2602254e3
Lorik Morina <lorik.morina1@student.uni-pr.edu>
2023-05-03 11:59:07 +0200

    #

---
+++ src/main/java/application/controllers/homeController.java
@@ -0,0 +1,30 @@
+package application.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class homeController {
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private TextField txtFullName;
+
+    @FXML
+    void btnLoginClick(ActionEvent event) {
+
+    }
+
+
+}
