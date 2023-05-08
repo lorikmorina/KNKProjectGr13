f4675b69210f912d2185a20ac598277df704f66f
Milot Qorrolli <milot.qorrolli@student.uni-pr.edu>
2023-05-08 12:23:47 +0200

    teacher sql

--- src/main/java/application/database/tables.sql
+++ src/main/java/application/database/tables.sql
@@ -24,3 +24,15 @@ CREATE TABLE children (
 );
 
 Select * from children;
+
+create table teachers (
+	id INT NOT NULL AUTO_INCREMENT,
+                         fullname VARCHAR(50) NOT NULL,
+                         email VARCHAR(50) NOT NULL,
+                         personalNr VARCHAR(50) NOT NULL,
+                         salted_hash VARCHAR(256) NOT NULL,
+                         salt VARCHAR(256) NOT NULL,
+                         PRIMARY KEY (id)
+);
+
+select * from teachers;
