ab16ded42ccfe10142d7fcbbcf9dabf499b9480a
Liridona <liridona.memeti@student.uni-pr.edu>
2023-05-17 23:49:28 +0200

    update in properties file

--- src/main/java/application/controllers/scheduleController.java
+++ src/main/java/application/controllers/scheduleController.java
@@ -137,7 +137,7 @@ public class scheduleController implements Initializable {
                 scheduleBtn.setText(bundle.getString("schedule.schedule.label"));
                 classScheduleBtn.setText(bundle.getString("classSchedule.schedule.label"));
                 logoutBtn.setText(bundle.getString("logout.button.schedule.text"));
-                searchField.setText(bundle.getString("search.placeholder.schedule = Search"));
+                searchField.setText(bundle.getString("search.placeholder.schedule"));
 
 
 
