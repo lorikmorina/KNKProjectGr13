cc03b82fc5e2571d42b9f3f48be40e226f69f983
LirakXhelili <lirakxhelili@gmail.com>
2023-05-18 10:01:33 +0200

    Made translation Home

--- src/main/java/application/controllers/homeController.java
+++ src/main/java/application/controllers/homeController.java
@@ -8,20 +8,25 @@ import application.service.UserSession;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class homeController {
+public class homeController implements Initializable {
 
 
     //    public homeController(User user) {
@@ -50,10 +55,63 @@ public class homeController {
     private Button manageButton;
     @FXML
     private Button classScheduleBtn;
+    @FXML
+    private Button homeHome;
+    @FXML
+    private Text totalChildrenEnrolled,totalTeacherEmployed,totalParentsRegistered;
+    @FXML
+    private RadioButton alButton,enButton;
 
     @FXML
     void btnLoginClick(ActionEvent event) {
 
+    }
+    public  void changeLanguage() {
+        ToggleGroup languageToggleGroup = new ToggleGroup();
+        alButton.setToggleGroup(languageToggleGroup);
+        enButton.setToggleGroup(languageToggleGroup);
+        languageToggleGroup.selectedToggleProperty().addListener((observable, oldToggle, newToggle) -> {
+            if(newToggle == alButton) {
+                Locale currentLocale = new Locale("sq", "AL");
+                ResourceBundle bundle = ResourceBundle.getBundle("translations.AL_SQ", currentLocale);
+                totalChildrenEnrolled.setText(bundle.getString("total.children.label"));
+                childrenEnrolled.setPadding(new Insets(0,0,0,20));
+                totalParentsRegistered.setText(bundle.getString("total.parents.label"));
+                totalTeacherEmployed.setText(bundle.getString("total.teachers.label"));
+
+                //MenuBar
+                homeHome.setText(bundle.getString("home.profile.label"));
+                manageButton.setText(bundle.getString("manage.profile.label.text"));
+                profileBtn.setText(bundle.getString("profile.profile.label.text"));
+                teacherManageBtn.setText(bundle.getString("teacher.profile.label.text"));
+                scheduleBtn.setText(bundle.getString("schedule.profile.label"));
+                classScheduleBtn.setText(bundle.getString("classSchedule.profile.label"));
+                logoutBtn.setText(bundle.getString("logout.button.profile.text"));
+
+
+            }else if(newToggle == enButton)  {
+                Locale currentLocale = new Locale("en", "US");
+                ResourceBundle bundle = ResourceBundle.getBundle("translations.US_EN", currentLocale);
+                totalChildrenEnrolled.setText(bundle.getString("total.children.label"));
+                totalParentsRegistered.setText(bundle.getString("total.parents.label"));
+                totalTeacherEmployed.setText(bundle.getString("total.teachers.label"));
+                childrenEnrolled.setPadding(new Insets(0,0,0,0));
+                //MenuBar
+                homeHome.setText(bundle.getString("home.profile.label"));
+                manageButton.setText(bundle.getString("manage.profile.label.text"));
+                profileBtn.setText(bundle.getString("profile.profile.label.text"));
+                teacherManageBtn.setText(bundle.getString("teacher.profile.label.text"));
+                scheduleBtn.setText(bundle.getString("schedule.profile.label"));
+                classScheduleBtn.setText(bundle.getString("classSchedule.profile.label"));
+                logoutBtn.setText(bundle.getString("logout.button.profile.text"));
+
+
+
+
+            }
+
+        });
+        languageToggleGroup.selectToggle(alButton);
     }
 
     public void initialize(UserSession session) throws SQLException {
@@ -237,6 +295,9 @@ public class homeController {
             e.printStackTrace();
         }
     }
-
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        changeLanguage();
+    }
 
 }
