48a544ea0ac61ab3fea786a2407763b32c71eb10
Hysen Lubovci <hysen.lubovci@student.uni-pr.edu>
2023-05-20 12:28:56 -0700

    translate ForgotPassword.

--- src/main/java/application/controllers/forgotPasswordController.java
+++ src/main/java/application/controllers/forgotPasswordController.java
@@ -1,9 +1,7 @@
 package application.controllers;
 
 import application.database.ConnectionUtil;
-import application.models.User;
-import application.models.Admin;
-import application.models.Teacher;
+import application.models.*;
 import application.models.User;
 import application.repository.AdminRepository;
 import application.repository.TeacherRepository;
@@ -54,7 +52,45 @@ public class forgotPasswordController implements Initializable{
     private TextField newPassword;
     @FXML
     private Label showLabel;
+    @FXML
+    private RadioButton alButton,enButton;
+    @FXML
+    private Hyperlink alrHaveAcc;
+    public  void changeLanguage() {
+        ToggleGroup languageToggleGroup = new ToggleGroup();
+        alButton.setToggleGroup(languageToggleGroup);
+        enButton.setToggleGroup(languageToggleGroup);
+        languageToggleGroup.selectedToggleProperty().addListener((observable, oldToggle, newToggle) -> {
+            if(newToggle == alButton) {
+                Locale currentLocale = new Locale("sq", "AL");
+                ResourceBundle bundle = ResourceBundle.getBundle("translations.AL_SQ", currentLocale);
+                forgotName.setPromptText(bundle.getString("fullname.txt"));
+                forgotEmail.setPromptText(bundle.getString("email.text"));
+                forgotPersonalNr.setPromptText(bundle.getString("personalNr.txt"));
+                newPassword.setPromptText(bundle.getString("newpassword.txt"));
+                secQuestion2.setPromptText(bundle.getString("securityquesion.txt"));
+                changeButton.setText(bundle.getString("changeButton.txt"));
+                alrHaveAcc.setText(bundle.getString("alrHaveAcc.txt"));
 
+            }else if(newToggle == enButton)  {
+                Locale currentLocale = new Locale("en", "US");
+                ResourceBundle bundle = ResourceBundle.getBundle("translations.US_EN", currentLocale);
+                forgotName.setPromptText(bundle.getString("fullname.txt"));
+                forgotEmail.setPromptText(bundle.getString("email.text"));
+                forgotPersonalNr.setPromptText(bundle.getString("personalNr.txt"));
+                newPassword.setPromptText(bundle.getString("newpassword.txt"));
+                secQuestion2.setPromptText(bundle.getString("securityquesion.txt"));
+                changeButton.setText(bundle.getString("changeButton.txt"));
+                alrHaveAcc.setText(bundle.getString("alrHaveAcc.txt"));
+            }
+        });
+        String selectedLanguage = LanguageManager.getInstance().getSelectedLanguage();
+        if (selectedLanguage.equals("sq_AL")) {
+            languageToggleGroup.selectToggle(alButton);
+        } else if (selectedLanguage.equals("en_US")) {
+            languageToggleGroup.selectToggle(enButton);
+        }
+    }
     public void initialize(){
         changeButton.setOnAction(this::initChangeButton);
     }
@@ -188,7 +224,7 @@ public class forgotPasswordController implements Initializable{
     }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        changeLanguage();
     }
 
 
